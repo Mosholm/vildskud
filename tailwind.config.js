69819034a48f51b9781cdfb00b5b50c20d505a6b
Triin Trempi <tri0022@stud.kea.dk>
2022-04-25 12:06:05 +0200

    updated landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bright-green": "#82FF77",
+        "bright-green-light": "#F2FFF2",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
   plugins: [require("daisyui")],
